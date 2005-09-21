54ef20e151ea710c4ed80d8243087cfb72952db7
droy <droy>
2005-09-21 17:38:30 +0000

    Initial WebMaster Commit

---
+++ platform-core/index.php
@@ -0,0 +1,24 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Platform Core</title>
+</head>
+<frameset rows="48,*" frameborder=0 framespacing=0 border="0">
+  <frame name="banner" scrolling="no" noresize target="home"
+                          src="../eproject-banner.html"  marginwidth="0" marginheight="0" frameborder="NO">
+  <frameset cols="126,*" frameborder=0 framespacing=0 border="0">
+    <frameset rows="220, *"  frameborder=0 framespacing=0 border="0">
+      <frame name="home_nav" scrolling="no" noresize target="main"
+                          src="../home_nav.html"  marginwidth="0" marginheight="0" frameborder="NO">
+      <frame name="nav" scrolling="no" noresize target="main"
+                          src="../eclipse/nav.html"  marginwidth="0" marginheight="0" frameborder="NO">
+    </frameset>
+    <frame name="main" marginwidth=10 marginheight=10 noresize frameborder="NO" src="http://dev.eclipse.org/viewcvs/index.cgi/%7Echeckout%7E/platform-core-home/main.html">
+  </frameset>
+  <noframes>
+  <body>
+  <p>This page uses frames, but your browser doesn't support them.</p>
+  </body>
+  </noframes> </frameset>
+</html>
