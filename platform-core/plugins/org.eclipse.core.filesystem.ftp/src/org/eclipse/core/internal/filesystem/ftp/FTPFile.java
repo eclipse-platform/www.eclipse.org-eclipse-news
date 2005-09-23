cc47039fa24afb3d3392826eb1796eca9f1c6235
johna <johna>
2005-09-23 17:56:11 +0000

    Initial release of sample FTP file system implementation.

---
+++ platform-core/plugins/org.eclipse.core.filesystem.ftp/src/org/eclipse/core/internal/filesystem/ftp/FTPFile.java
@@ -0,0 +1,230 @@
+/**********************************************************************
+ * Copyright (c) 2005 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.core.internal.filesystem.ftp;
+
+import java.io.*;
+import java.net.*;
+import org.eclipse.core.filesystem.*;
+import org.eclipse.core.filesystem.provider.FileStore;
+import org.eclipse.core.runtime.*;
+import org.eclipse.ftp.*;
+import org.eclipse.ftp.internal.FTPDirectoryEntry;
+
+/**
+ * Implementation of FileStore for accessing files using file transfer protocol (FTP).
+ */
+public class FTPFile extends FileStore {
+	protected final IPath relativePath;
+	protected final URL root;
+
+	public FTPFile(URL root, IPath relativePath) {
+		this.root = root;
+		this.relativePath = relativePath;
+	}
+
+	public IFileInfo[] childInfos(int options, IProgressMonitor monitor) {
+		final IDirectoryEntry[] entries = listFiles(monitor, false);
+		if (entries == null)
+			return EMPTY_FILE_INFO_ARRAY;
+		IFileInfo[] infos = new IFileInfo[entries.length];
+		for (int i = 0; i < entries.length; i++) {
+			infos[i] = FTPUtil.entryToFileInfo(entries[i]);
+		}
+		return infos;
+	}
+
+	public String[] childNames(int options, IProgressMonitor monitor) {
+		final IDirectoryEntry[] entries = listFiles(monitor, false);
+		if (entries == null)
+			return EMPTY_STRING_ARRAY;
+		String[] names = new String[entries.length];
+		for (int i = 0; i < entries.length; i++) {
+			names[i] = entries[i].getName();
+		}
+		return names;
+	}
+
+	private IFileInfo createMissingInfo(String myName) {
+		IFileInfo info = FileSystemCore.createFileInfo();
+		info.setName(myName);
+		info.setExists(false);
+		return info;
+	}
+
+	public void delete(int options, IProgressMonitor monitor) throws CoreException {
+		FTPUtil.run(root, new IFtpRunnable() {
+			public void run(IProgressMonitor pm) throws FtpException {
+				if (fetchInfo().isDirectory()) {
+					FTPUtil.getClient().deleteDirectory(relativePath.toString(), pm);
+				} else {
+					FTPUtil.getClient().deleteFile(relativePath.toString(), pm);
+				}
+			}
+		}, monitor);
+	}
+
+	public IFileInfo fetchInfo(int options, IProgressMonitor monitor) {
+		if (relativePath.isEmpty())
+			return fetchRootInfo(options, monitor);
+		String myName = getName();
+		try {
+			IFileInfo[] infos = getParent().childInfos(options, monitor);
+			for (int i = 0; i < infos.length; i++) {
+				if (myName.equals(infos[i].getName()))
+					return infos[i];
+			}
+		} catch (CoreException e) {
+			//ignore and fall through below
+		}
+		//this file doesn't exist - that's fine
+		return createMissingInfo(myName);
+	}
+
+	/**
+	 * Since the root element has no parent directory, we must
+	 * list its own contents and then return the directory information
+	 * for the self reference directory entry (".").
+	 * 
+	 * @param options
+	 * @param monitor
+	 * @return File info for the root element
+	 */
+	private IFileInfo fetchRootInfo(int options, IProgressMonitor monitor) {
+		IDirectoryEntry[] entries = listFiles(monitor, true);
+		for (int i = 0; i < entries.length; i++) {
+			if (entries[i].getName().equals(".")) {//$NON-NLS-1$
+				IFileInfo info = FTPUtil.entryToFileInfo(entries[i]);
+				info.setName(getName());
+				return info;
+			}
+		}
+		return createMissingInfo(getName());
+	}
+
+	public IFileStore getChild(String name) {
+		return new FTPFile(root, relativePath.append(name));
+	}
+
+	public IFileSystem getFileSystem() {
+		return FTPFileSystem.getInstance();
+	}
+
+	public String getName() {
+		if (!relativePath.isEmpty())
+			return relativePath.lastSegment();
+		//this is the root resource, so use the last segment of the URL path
+		String pathPart = root.getPath();
+		int offset = pathPart.lastIndexOf('/');
+		if (offset >= 0)
+			return pathPart.substring(offset);
+		//if there is only one segment, return it
+		if (pathPart.length() > 0)
+			return pathPart;
+		return root.getHost();
+	}
+
+	public IFileStore getParent() {
+		if (!relativePath.isEmpty())
+			return new FTPFile(root, relativePath.removeLastSegments(1));
+		//the root has no parent
+		return null;
+	}
+
+	private IDirectoryEntry[] listFiles(IProgressMonitor monitor, final boolean includeParents) {
+		final IDirectoryEntry[][] entries = new FTPDirectoryEntry[1][0];
+		try {
+			FTPUtil.run(root, new IFtpRunnable() {
+				public void run(IProgressMonitor pm) throws FtpException {
+					entries[0] = FTPUtil.getClient().listFiles(relativePath.toString(), includeParents, pm);
+				}
+			}, monitor);
+		} catch (FtpException e) {
+			Policy.log(IStatus.ERROR, "Error listing files from FTP file", e); //$NON-NLS-1$
+		}
+		return entries[0];
+	}
+
+	public IFileStore mkdir(int options, IProgressMonitor monitor) throws CoreException {
+		//FTP fails a create attempt when the file or directory already exists,
+		//so we must first check for existence.
+		final IFileInfo fileInfo = fetchInfo();
+		if (fileInfo.exists()) {
+			if (!fileInfo.isDirectory()) {
+				String message = "A directory could not be created because a file exists with the same name: " + toString();
+				Policy.error(IFileStoreConstants.WRONG_TYPE_LOCAL, message);
+			}
+			return this;
+		}
+
+		//create a directory
+		FTPUtil.run(root, new IFtpRunnable() {
+			public void run(IProgressMonitor pm) throws FtpException {
+				FTPUtil.getClient().createDirectory(relativePath.toString(), pm);
+			}
+		}, monitor);
+		return this;
+	}
+
+	public InputStream openInputStream(int options, IProgressMonitor monitor) throws CoreException {
+		final InputStream[] result = new InputStream[1];
+		FTPUtil.run(root, new IFtpRunnable() {
+			public void run(IProgressMonitor pm) throws FtpException {
+				result[0] = FTPUtil.getClient().getFile(relativePath.toString(), true, 0, pm);
+			}
+		}, monitor);
+		return result[0];
+	}
+
+	public OutputStream openOutputStream(int options, final IProgressMonitor monitor) throws CoreException {
+		return new ByteArrayOutputStream() {
+			public void close() throws IOException {
+				byte[] bytes = toByteArray();
+				try {
+					FTPFile.this.write(new ByteArrayInputStream(bytes), bytes.length, monitor);
+				} catch (CoreException e) {
+					throw new IOException(e.getMessage());
+				}
+			}
+		};
+	}
+
+	public void putInfo(IFileInfo info, int options, IProgressMonitor monitor) throws CoreException {
+		Policy.error(IFileStoreConstants.FAILED_WRITE_LOCAL, "The putInfo method is not implemented");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.filesystem.IFileStore#toString()
+	 */
+	public String toString() {
+		StringBuffer result = new StringBuffer(root.toString());
+		int len = result.length();
+		//make sure there is a slash separating root and relative path
+		if (len > 0 && result.charAt(len - 1) != '/')
+			result.append('/');
+		result.append(relativePath.toString());
+		return result.toString();
+	}
+
+	public URI toURI() {
+		try {
+			return new URI("ftp", root.toExternalForm(), relativePath.toString(), null); //$NON-NLS-1$
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	void write(final InputStream stream, final int size, IProgressMonitor monitor) throws CoreException {
+		FTPUtil.run(root, new IFtpRunnable() {
+			public void run(IProgressMonitor pm) throws FtpException {
+				FTPUtil.getClient().putFile(relativePath.toString(), stream, size, true, pm);
+			}
+		}, monitor);
+	}
+}
---
+++ platform-core/plugins/org.eclipse.core.filesystem.ftp/src/org/eclipse/core/internal/filesystem/ftp/FTPFileSystem.java
@@ -0,0 +1,41 @@
+/**********************************************************************
+ * Copyright (c) 2005 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.core.internal.filesystem.ftp;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.IFileSystem;
+import org.eclipse.core.filesystem.provider.FileSystem;
+import org.eclipse.core.runtime.Path;
+
+/**
+ * File system implementation for the "ftp" URI scheme.
+ */
+public class FTPFileSystem extends FileSystem {
+	private static IFileSystem instance;
+
+	public static IFileSystem getInstance() {
+		return instance;
+	}
+
+	public FTPFileSystem() {
+		super();
+		instance = this;
+	}
+
+	public IFileStore getStore(URI uri) {
+		try {
+			return new FTPFile(uri.toURL(), Path.ROOT);
+		} catch (MalformedURLException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+	}
+}
