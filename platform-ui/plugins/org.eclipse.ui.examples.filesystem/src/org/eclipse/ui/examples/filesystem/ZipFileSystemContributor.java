bf5001d0c967bb8731ecdd0b299216d87f56781f
tod <tod>
2006-01-18 18:59:23 +0000

    Added zip file example

---
+++ platform-ui/plugins/org.eclipse.ui.examples.filesystem/src/org/eclipse/ui/examples/filesystem/ZipFileSystemContributor.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ui.examples.filesystem;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.core.internal.filesystem.zip.ZipFileSystem;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.ide.fileSystem.FileSystemContributor;
+
+/**
+ * ZipFileSystemContributor is the zip example of a file system
+ * contributor.
+ *
+ */
+public class ZipFileSystemContributor extends FileSystemContributor {
+
+	public ZipFileSystemContributor() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.ide.fileSystem.FileSystemContributor#browseFileSystem(java.lang.String, org.eclipse.swt.widgets.Shell)
+	 */
+	public URI browseFileSystem(String initialPath, Shell shell) {
+		FileDialog dialog = new FileDialog(shell);
+		
+		if (initialPath.length() > 0) 
+			dialog.setFilterPath(initialPath);
+		
+
+		String selectedFile = dialog.open();
+		if (selectedFile == null)
+			return null;
+		try {
+			return new URI(ZipFileSystem.SCHEME_ZIP, null, "/", selectedFile, null);
+		} catch (URISyntaxException e) {
+			return null;
+		}
+	}
+
+}
