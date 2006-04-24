c1ad26dfdfb6cd44c0c3a96314d8b5e8f14b9662
johna <johna>
2006-04-24 18:09:31 +0000

    added convenience methods for logging and error handling

--- platform-ui/plugins/org.eclipse.ui.examples.filesystem/src/org/eclipse/core/internal/filesystem/memory/Policy.java
+++ platform-ui/plugins/org.eclipse.ui.examples.filesystem/src/org/eclipse/core/internal/filesystem/memory/Policy.java
@@ -11,18 +11,29 @@
 package org.eclipse.core.internal.filesystem.memory;
 
 import org.eclipse.core.runtime.*;
+import org.eclipse.jface.dialogs.ErrorDialog;
 
 /**
  * 
  */
 public class Policy {
+	public static final String PI_FILESYSTEM_EXAMPLE = "org.eclipse.ui.examples.filesystem"; //$NON-NLS-1$
 
 	public static void error(String message) throws CoreException {
-		throw new CoreException(new Status(IStatus.ERROR, "org.eclipse.core.tests.resources", 1, message, null));
+		throw new CoreException(new Status(IStatus.ERROR, PI_FILESYSTEM_EXAMPLE, 1, message, null));
 	}
 
 	private Policy() {
 		super();
+	}
+	public static void log(IStatus status) {
+		Platform.getLog(Platform.getBundle(PI_FILESYSTEM_EXAMPLE)).log(status);
+	}
+	
+	public static IStatus createStatus(Throwable t) {
+		if (t instanceof CoreException) 
+			return ((CoreException)t).getStatus();
+		return new Status(IStatus.ERROR, PI_FILESYSTEM_EXAMPLE, 1, "Internal Error: " + t.getMessage(), t);
 	}
 
 }
