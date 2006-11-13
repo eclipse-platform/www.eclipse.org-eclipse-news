54dcf67c8f754cdef2a38ffb94d90d1526e683ab
mrennie <mrennie>
2006-11-13 07:55:43 +0000

    *** empty log message ***

--- debug/nan/3.2/M2/nan.php
+++ debug/nan/3.2/M2/nan.php
@@ -27,35 +27,6 @@
 		  <tr> 
 		    <td colspan="2"> <hr> </td>
 		  </tr>
-		  <tr> 
-		    <td valign="top" align="left"> <p align="right"><b>Breakpoint import and export</b></td>
-		    <td valign="top"> <p>Breakpoints can be exported to and imported from the 
-		        local file system. The export wizard allows you to select the breakpoints 
-		        to export. Breakpoint working sets can optionally be created when importing 
-		        breakpoints.</p>
-		      <p align="center"><img src="brkpt_export.PNG" width="439" height="484"></p></td>
-		  </tr>
-		  <tr> 
-		    <td valign="top" align="left"> <p align="right"><b>System property launch 
-		        variable</b></td>
-		    <td valign="top"> <p>A new launch variable allows you to dynamically specify 
-		        system properties as program and VM arguments from the Eclipse runtime. 
-		        The variable accepts a single argument which is the name of the system 
-		        property to be resolved at launch time. For example, &quot;${system_property:java.home}&quot;.</p>
-		      <p align="center"><img src="sys_prop.PNG"></p>
-		      <p>&nbsp;</p>
-		      <p>&nbsp;</p></td>
-		  </tr>
-		  <tr> 
-		    <td width="30%" valign="top" align="left"> <p align="right"><b>Evaluation 
-		        support for arrays</b></td>
-		    <td width="70%" valign="top"> <p>Content assist and evaluations are now supported 
-		        for arrays. The debugger allows you to refer to the selected array as 
-		        <i>this</>. </p>
-		      <p align="center"><img src="arr_eval.PNG" width="453" height="381"></p>
-		      <p>&nbsp;</p>
-		      <p>&nbsp;</p></td>
-		  </tr>
 		</table>
 	</div>
 	<div id="rightcolumn">
