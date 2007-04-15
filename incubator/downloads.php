e271126cbc868b09877262ee491913096694111d
jeff <jeff>
2007-04-15 03:07:14 +0000

    *** empty log message ***

--- incubator/downloads.php
+++ incubator/downloads.php
@@ -39,11 +39,9 @@ pages:
  <div class="sideitem">
    <h6>Incubation</h6>
    <div align="center">
-      <p>&nbsp;</p>
       <a href="/projects/what-is-incubation.php"><img 
         align="center" src="/images/egg-incubation.png" 
         border="0" alt="Incubation" /></a>
-      <p>&nbsp;</p>
    </div>
  </div>
 </div>
