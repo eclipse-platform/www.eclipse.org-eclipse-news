bae3f70503caa3578b75954b1a3757bf7e686727
kmoir <kmoir>
2007-11-08 18:43:19 +0000

    phoenixify releng page

--- platform-releng/index.php
+++ platform-releng/index.php
@@ -30,39 +30,22 @@ ob_start();
 		  <h3>About the Platform Releng</h3>
 			<p>Platform releng provides release engineering services for the Eclipse Project team (<a href="http://www.eclipse.org/platform=">Platform"</a>,
 			<a href="http://www.eclipse.org/jdt=">JDT"</a><a href="http://www.eclipse.org/pde=">PDE"</a><a href="http://www.eclipse.org/equinox=">Equinox"</a>).  We maintain 
-			the build scripts that are used to 	massage the source from the developer to a download at eclipse.org.  We are responsible the following cvs projects
+			the build scripts that are used to massage the source from the developer to a download at eclipse.org.  We are responsible the following cvs projects
 			</p>
 			<ul>
 				<li><b>org.eclipse.releng</b> - Map files used by the entire team to submit source to the build</li>
-				<li><b>org.releng.eclipsebuilder</b> - Scripts used to run the build and run tests.</li>
+				<li><b>org.releng.eclipsebuilder</b> - Scripts used to run the build and tests.</li>
 				<li><b>org.eclipse.releng.basebuilder</b> - A subset of SDK plugins used to run the build.</li>
 				<li><b>org.eclipse.test</b> - Eclipse test framework</li>
-				<li><b>org.eclipse.releng.tests</b> - Test eclipse plugin and features for inclusion of legal files,  adherence to versioning guidelines and readiness translation.</li>
-			</ul>
-			<p>The RelEng Plug-in is included in the Eclipse builds and is available at the bottom of the download page. <br>
-      </p>
-       </p><p>This Plug-in provides  features that will help with the Eclipse development process. Installing the plug-in  will add the following actions. The source is contained in the *.jar files so 
-please feel free to submit patches for features or bug fixes. To install simply unzip the file into your plugins directory and restart Eclipse. </p>
-b>Please use the Release feature of this plug-in to do your build submissions.</b></p><ol> 
-<li><b>Release</b> to the Team menu. This action will Tag selected projects with  the specified version <b>and</b> update the appropriate loaded *.map files with  the version. The user must have the *.map files loaded in their workspace and 
-the use must commit the map file changes to the repository when done.</li>
-<li><b>Load  Map Projects </b>to the Team menu. Select one or more *.map file and this action will load the projects listed in the *.map file into your workspace. Naturally 
-the versions specified in the *.map file will be loaded.</li>
-<li><b>Tag Map Projects</b> to the Team menu. Select one or more *Map files and this action will tag the projects 
-listed in the *Map files with a tag you specify.</li>
-<li><b>Compare with Released</b>  to the Compare menu. Compare the selected projects with the versions referenced in the currently loaded map files.</li>
-<li><b>Replace with Released</b> to the Replace menu. Replace the selected projects with the versions referenced in the currently loaded map files.</li>
-<li><b>Fix Copyright</b> to the Resource Perspective Projects context menu. Select one or more projects in the Resource Perspective. This action will sanity check the copyright notices in all the *.java and *.properties 
-files. Copyrights will be updated automatically where the tool deems appropriate.  A copyright.log file will be written to the workspace directory noting odd conflicts 
-that need to be looked at. You need to commit the changes yourself. 
-		    </p>
+				<li><b>org.eclipse.releng.tests</b> - Test eclipse plugin and features for inclusion of legal files and adherence to versioning guidelines.</li>
+			</ul>			
 	    </div>
 		<div class="homeitem3col">
 		  <h3>Get involved!</h3>
 	      <p>If you are interested in seeing what's happening in platform-releng, subscribe to our mailing list:
 	        <a href="http://dev.eclipse.org/mailman/listinfo/platform-releng-dev">platform-releng-dev@eclipse.org</a>. 
-	        There are also a number of bugs with the "helpwanted" or "bugday" tag that we are looking for the community to help out with</p>
-			<b>Bugs</b> <ul>
+	        There are also a number of bugs with the "helpwanted" tag that indicate where help would be greatly appreciated.</p>
+			
 
         <li><a href="http://bugs.eclipse.org/bugs/buglist.cgi?bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&priority=P1&email1=&emailtype1=substring&emailassigned_to1=1&email2=&emailtype2=substring&emailreporter2=1&bugidtype=include&bug_id=&changedin=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&product=Platform&version=&component=Releng&short_desc=&short_desc_type=allwordssubstr&long_desc=&long_desc_type=allwordssubstr&keywords=&keywords_type=anywords&field0-0-0=noop&type0-0-0=noop&value0-0-0=&cmdtype=doit&order=Reuse+same+sort+as+last+time" target="_top">Priority 
           1</a></li>
@@ -79,7 +62,7 @@ that need to be looked at. You need to commit the changes yourself.
 
 	<div id="rightcolumn">
 		<div class="sideitem">
-			<h6>Platform releng</h6> links</h6>
+			<h6>Platform releng link</h6> 
 			<ul>
 				<li><a href="index.php">Home</a></li>
 				<li><a href="buildSchedule.html">Build Schedule</a></li>
