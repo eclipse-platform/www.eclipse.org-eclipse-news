1d78ca01aef0777fcd743520d2b8d0d637c49b83
tod <tod>
2008-02-08 21:09:57 +0000

    Updated for M5

---
+++ platform-ui/plugins/org.eclipse.ui.ide.examples.markers/src/org/eclipse/ui/ide/examples/markers/ExampleMarkersView.java
@@ -0,0 +1,15 @@
+package org.eclipse.ui.ide.examples.markers;
+
+import org.eclipse.ui.views.markers.MarkerSupportView;
+
+public class ExampleMarkersView extends MarkerSupportView {
+
+	/**
+	 * Open a new instance of the receiver.
+	 */
+	public ExampleMarkersView() {
+		super("org.eclipse.ui.ide.examples.markers.exampleGenerator");
+		
+	}
+
+}
