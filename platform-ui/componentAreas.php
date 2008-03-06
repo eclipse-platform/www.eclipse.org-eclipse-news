7fd8add035048ed9cf5fe8b6816bdee80a1be14a
khorne <khorne>
2008-03-06 13:53:11 +0000

    Updated component area doc

--- platform-ui/componentAreas.php
+++ platform-ui/componentAreas.php
@@ -1,3 +1,33 @@
+<style type="text/css">
+<!--
+div.component{
+	vertical-align: top; 
+	text-align:right;
+}
+a.bugs {
+	style="font-size: smaller";
+}
+div.owner {
+	text-indent: 30px;
+}
+-->
+</style>
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	$pageTitle = "Platform UI Component Areas";
+	$pageKeywords	= "platform ui component area";
+	$pageAuthor = "Platform UI Monkey";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+	
+	$script = <<<EOHTML 
 <script language="javascript">
 <!--
 function viewBugsWithSummary(desc) {
@@ -67,35 +97,8 @@ function buildTable(loadedJSON){
 //-->
 <!-- 'directories=0,height=480,location=0,resizable=1,scrollbars=1,toolbar=0,width=515' -->
 </script>
-<style type="text/css">
-<!--
-div.component{
-	vertical-align: top; 
-	text-align:right;
-}
-a.bugs {
-	style="font-size: smaller";
-}
-div.owner {
-	text-indent: 30px;
-}
--->
-</style>
 
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-
-	$pageTitle = "Platform UI Component Areas";
-	$pageKeywords	= "platform ui component area";
-	$pageAuthor = "Platform UI Monkey";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-
-	# End: page-specific settings
-	#
+EOHTML;
 		
 	# Paste your HTML content between the EOHTML markers!	
 	$html = <<<EOHTML
@@ -124,5 +127,6 @@ EOHTML;
 
 
 	# Generate the web page
+	$App->AddExtraHtmlHeader($script);
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
