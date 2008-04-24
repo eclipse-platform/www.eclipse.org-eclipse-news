253b3a182092a5c972f08fb18af10a5ae0ac6fd1
khorne <khorne>
2008-04-24 18:19:32 +0000

    *** empty log message ***

---
+++ platform-ui/dbinfo.php
@@ -0,0 +1,29 @@
+<?php
+require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
+$dbc 	= new DBConnectionBugs();
+$dbh 	= $dbc->connect();
+$rs = null;
+
+$sql_info = "SELECT  id, name FROM products";
+
+$rs = mysql_query($sql_info, $dbh);
+
+echo "<h1>Products</h1>";
+echo "<table border='1' cellpadding='2' width='80%'>";
+echo "<tr><th>ID</th><th>Name</th></tr>";
+
+while ($myrow  = mysql_fetch_assoc($rs)) {
+	echo "<tr><td>" . $myrow['id'] . "</td><td>" . $myrow['name'] . "</td></tr>";
+	
+}
+
+echo "</table>";
+
+$dbc->disconnect();
+
+$rs 		= null;
+$dbh 		= null;
+$dbc 		= null;
+
+exit();
+?>
