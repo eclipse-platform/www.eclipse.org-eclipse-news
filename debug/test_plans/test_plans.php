a8c6799628d86fea3ed7f8a9b1d66fccf57518f6
mrennie <mrennie>
2008-05-16 14:54:26 +0000

    updated test plan for 3.4rc1

--- debug/test_plans/test_plans.php
+++ debug/test_plans/test_plans.php
@@ -47,6 +47,7 @@
 				<li><a href="/eclipse/debug/test_plans/test_plan-3.4M5.php">3.4M5</a></li>
 				<li><a href="/eclipse/debug/test_plans/test_plan-3.4M6.php">3.4M6</a></li>
 				<li><a href="/eclipse/debug/test_plans/test_plan-3.4M7.php">3.4M7</a></li>
+				<li><a href="/eclipse/debug/test_plans/test_plan-3.4RC1.php">3.4RC1</a></li>
 			</ul>
 		</div>
 		<div class="homeitem3col">
