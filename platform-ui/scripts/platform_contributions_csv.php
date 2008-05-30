327608f3699f8e8a02503e2036057a7e63b8d565
khorne <khorne>
2008-05-30 17:28:26 +0000

    *** empty log message ***

--- platform-ui/scripts/platform_contributions_csv.php
+++ platform-ui/scripts/platform_contributions_csv.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>$CVSHeader: www/eclipse/platform-ui/scripts/platform_contributions_csv.php,v 1.6 2008/05/30 17:27:42 khorne Exp $</title>
+<title>$CVSHeader: www/eclipse/platform-ui/scripts/platform_contributions_csv.php,v 1.7 2008/05/30 17:28:26 khorne Exp $</title>
 </head>
 <body>
 <?php
@@ -154,7 +154,7 @@ $count = countAddedLines($myrow);
 echo $count < 100 ? "small" :  $count . ",";
 
 echo $committer . ",";
-echo str_replace(",", " ", $myrow['description'];
+echo str_replace(",", " ", $myrow['description']);
 echo "\n";
 }
 }
