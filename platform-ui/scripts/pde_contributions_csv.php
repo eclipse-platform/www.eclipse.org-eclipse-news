0abf6b944a990ad1081a7794e7ef982959ee6df3
darin <darin>
2008-06-02 19:39:19 +0000

    added Wassim to committer list

--- platform-ui/scripts/pde_contributions_csv.php
+++ platform-ui/scripts/pde_contributions_csv.php
@@ -1,16 +1,16 @@
 <html>
 <head>
-<title>$CVSHeader: www/eclipse/platform-ui/scripts/pde_contributions_csv.php,v 1.7 2008/06/02 19:31:47 darin Exp $</title>
+<title>$CVSHeader: www/eclipse/platform-ui/scripts/pde_contributions_csv.php,v 1.8 2008/06/02 19:39:19 darin Exp $</title>
 </head>
 <body>
 <?php
 require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
 ini_set("display_errors", "true");
 error_reporting (E_ALL);
-$committerList = array("Curtis Windatt" => array("IBM", ""), "Olivier Thomann" => array("IBM", ""), "Darin Wright" => array("IBM", ""), "Michael Rennie" => array("IBM", ""), "Brian Bauman"=> array("IBM", ""), "Noam Chitayat"=> array("IBM", ""), "Chris Aniszczyk" => array("IBM", "") );
+$committerList = array("Wassim Melhem" => array("IBM", ""), "Curtis Windatt" => array("IBM", ""), "Olivier Thomann" => array("IBM", ""), "Darin Wright" => array("IBM", ""), "Michael Rennie" => array("IBM", ""), "Brian Bauman"=> array("IBM", ""), "Noam Chitayat"=> array("IBM", ""), "Chris Aniszczyk" => array("IBM", "") );
 // the following bugs have been examined by hand and found to not be viable contributions
 $exclusions = array();
-$committerOverrides = array("109137" => "Wassim Melhem", "232857" => "Chris Aniszczyk", "232706" => "Chris Aniszczyk", "231977" => "Chris Aniszczyk", "230447" => "Chris Aniszczyk", "230332"  => "Chris Aniszczyk", "230248" => "Chris Aniszczyk", "230172" => "Chris Aniszczyk", "229453" => "Nick Boldt", "227105" => "Chris Aniszczyk", "226800" => "Chris Aniszczyk", "226590" => "Chris Aniszczyk", "226352" => "Chris Aniszczyk", "226238" => "Chris Aniszczyk");
+$committerOverrides = array("232857" => "Chris Aniszczyk", "232706" => "Chris Aniszczyk", "231977" => "Chris Aniszczyk", "230447" => "Chris Aniszczyk", "230332"  => "Chris Aniszczyk", "230248" => "Chris Aniszczyk", "230172" => "Chris Aniszczyk", "229453" => "Nick Boldt", "227105" => "Chris Aniszczyk", "226800" => "Chris Aniszczyk", "226590" => "Chris Aniszczyk", "226352" => "Chris Aniszczyk", "226238" => "Chris Aniszczyk");
 $contributorOverrides = array();
 $locationOverrides = array();
 $includedMilestones = array("3.4", "3.4 M1", "3.4 M2", "3.4 M4", "3.4 M5", "3.4 M6", "3.4 M7", "3.4 RC1", "3.4 RC2", "3.4 RC3", "3.4 RC4");
