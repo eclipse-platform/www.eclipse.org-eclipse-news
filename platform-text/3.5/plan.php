7a6366ebada01e7bc385cf9dc17b625db9e8774c
dmegert <dmegert>
2008-07-02 09:40:17 +0000

    Initial 3.5 plan.

---
+++ platform-text/3.5/plan.php
@@ -0,0 +1,31 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Platform Text 3.5 Plan";
+	$pageKeywords	= "text, editor, Java, plan, planning, JDT, jdt, development, tools, ide, Eclipse";
+	$pageAuthor		= "The Text Team";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+
+ob_start();
+?>
+		
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+		<?php include("plan.html"); ?>
+	</div>
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
