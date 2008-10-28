33a427f1feb9338c38a400fcfca7ed7d969bd142
mrennie <mrennie>
2008-10-28 18:03:41 +0000

    trying to remove style

--- debug/test_plans/test_plan-3.5.php
+++ debug/test_plans/test_plan-3.5.php
@@ -320,11 +320,11 @@
 				<ul>
 					<li> All Instances
 						<ul>
-							<li style="none">Should not be available when not debugging with 1.6 VM</li>
-							<li style="none">Variables view popup</li>
-							<li style="none">Java Outline view popup (type and constructors only)</li>
-							<li style="none">Java editor popup (type name or constructor name selected)</li>
-							<li style="none">Change number of instances to show on Heap walking preference page</li>
+							<li style="">Should not be available when not debugging with 1.6 VM</li>
+							<li style="">Variables view popup</li>
+							<li style="">Java Outline view popup (type and constructors only)</li>
+							<li style="">Java editor popup (type name or constructor name selected)</li>
+							<li style="">Change number of instances to show on Heap walking preference page</li>
 						</ul>
 					</li>
 					<li> All References
