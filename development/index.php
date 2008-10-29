531503868f1bd3722f9a61c920cfa5d54af7d8ab
johna <johna>
2008-10-29 19:26:40 +0000

    Added Galileo plan

--- development/index.php
+++ development/index.php
@@ -35,8 +35,8 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Releases currently under development</h3>
-	        <li><a href="freeze_plan_3_4_1.php"> Eclipse Project Ganymede SR1 Endgame Plan</a><br>
-	    		Information about how we plan to wrap up Ganymede Service Release 1 (version 3.4.1).</li>     
+	      <li><a href="http://www.eclipse.org/projects/project-plan.php?projectid=eclipse"> Eclipse Project Galileo Plan</a><br>
+	        This document lays out the feature and API set for the Galileo (3.5) release of the Eclipse Project. </li>
     <ul>
     </ul>
       	</div>
@@ -103,6 +103,8 @@ ob_start();
 		<div class="homeitem3col">
 			<h3>Historical information about past releases</h3>			
 			<ul>
+	        <li><a href="freeze_plan_3_4_1.php"> Eclipse Project Ganymede SR1 Endgame Plan</a><br>
+	    		Information about how we plan to wrap up Ganymede Service Release 1 (version 3.4.1).</li>     
 	      <li><a href="eclipse_project_plan_3_4.html"> Eclipse Project 3.4 Plan</a><br>
 	        This document lays out the feature and API set for the 3.4 release of Eclipse Project. </li>
 	        <li><a href="freeze_plan_3.4.php"> Eclipse Project 3.4 Endgame Plan</a><br>
