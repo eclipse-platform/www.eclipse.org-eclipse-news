8c26d5abc9f79d16a083d0301fcef4af1d1bb4fc
bbokowski <bbokowski>
2008-12-08 16:50:36 +0000

    team photos

--- platform-ui/team/index.php
+++ platform-ui/team/index.php
@@ -29,8 +29,10 @@
 <script src="drag.js">
 </script>
 <div width="100%" height="100%">
-<img class="piece" style="position: absolute; left: 200px; top: 200px;"
+<img class="piece" style="position: absolute; left: 300px; top: 200px;"
   src="images/boris-head-24-fs8.png" onmouseover="handleMouseOver(this, 'boris');" onmousedown="handleDragStart(event);" /> 
+<img class="piece" style="position: absolute; left: 431px; top: 200px;"
+  src="images/nose-fs8.png" onmouseover="handleMouseOver(this, 'nose');" onmousedown="handleDragStart(event);" /> 
 </div>
 EOHTML;
 
