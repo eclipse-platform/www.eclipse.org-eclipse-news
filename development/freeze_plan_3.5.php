689a79fa11351621f5fa0490a02a958611ce5bfb
johna <johna>
2009-04-03 03:10:21 +0000

    *** empty log message ***

--- development/freeze_plan_3.5.php
+++ development/freeze_plan_3.5.php
@@ -32,14 +32,10 @@
 	# Paste your HTML content between the markers!	
 ob_start();
 ?>		
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+		<div class="homeitem3col">
 
-<head>
-<title>Eclipse 3.5 Endgame Plan</title>
-</head>
-<body>
-<div id="midcolumn">
-<div class="homeitem3col">
-<h3>Eclipse 3.5 Draft endgame plan</h3>
 <table cellspacing="5" cellpadding="2" width="100%" border="0">
 <tbody> <tr> <td valign="top" align="left" colspan="2"><b>Status</b></td></tr>
 <tr> <td valign="top" align="right"></td>
