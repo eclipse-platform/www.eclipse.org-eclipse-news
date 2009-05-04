8d1b09abe4458bf71628777c66e6965b6830a28c
johna <johna>
2009-05-04 18:04:55 +0000

    Updated status

--- development/freeze_plan_3.5.php
+++ development/freeze_plan_3.5.php
@@ -36,7 +36,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>April 2009 - M7 development underway.</b></p>
+			<p><b>May 4, 2009 - RC1 development underway.</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
