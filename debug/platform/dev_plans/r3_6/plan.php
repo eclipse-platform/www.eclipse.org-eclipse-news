42ce6690ff38a0c80233c9846a7b06e3f9055805
mrennie <mrennie>
2009-08-10 17:13:55 +0000

    updated web page

--- debug/platform/dev_plans/r3_6/plan.php
+++ debug/platform/dev_plans/r3_6/plan.php
@@ -1,7 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Combined Platform and JDT Debug 3.5 Plan";
+	$pageTitle 		= "Combined Platform and JDT Debug 3.6 Plan";
 	$pageKeywords	= "debug, Java, plan, planning, JDT, jdt, development, tools, ide, Eclipse";
 	$pageAuthor		= "The Debug Team";
 	
