1f60a208dfc88bad7505b23cca9731f51339e23a
mrennie <mrennie>
2009-08-10 16:41:05 +0000

    updated from link to test section

--- debug/index.php
+++ debug/index.php
@@ -85,7 +85,7 @@
 				<li>To help new contributors, we have created a <a href="/eclipse/debug/documents/project_sets/debug.psf" target="_blank">project set file</a> containing all of the projects needed to get started with debug coding.
 				There is also a project set file for <a href="/eclipse/debug/documents/project_sets/debug3.3.1.psf" target="_blank">3.3.1 maintenance</a>.</li>
 				<li>Debug overall <a href="/eclipse/debug/platform/dev_plans/r3_6/plan.php">3.6 plan</a></li>
-				<li><a href="/eclipse/debug/test_plans/test_plans.php#3-5_testing">Debug 3.6 Test Matrix</a></li>
+				<li><a href="/eclipse/debug/test_plans/test_plans.php#3-6_testing">Debug 3.6 Test Matrix</a></li>
 			</ul>
 		</div>
 	</div>
