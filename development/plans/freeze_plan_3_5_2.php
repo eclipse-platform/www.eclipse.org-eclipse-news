565f496ce19bdf681d22ce5d41c5dad43cf44558
johna <johna>
2010-01-21 15:29:20 +0000

    status updates

--- development/plans/freeze_plan_3_5_2.php
+++ development/plans/freeze_plan_3_5_2.php
@@ -37,7 +37,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>January 20, 2010 - building to produce RC2</b></p>
+			<p><b>January 21, 2010 - Full day test pass against RC2 build</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
