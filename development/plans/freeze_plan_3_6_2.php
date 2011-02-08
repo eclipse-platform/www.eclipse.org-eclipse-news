eff27cafd148c071995f96542bd9cbfe03b66242
johna <johna>
2011-02-08 20:44:04 +0000

    status update

--- development/plans/freeze_plan_3_6_2.php
+++ development/plans/freeze_plan_3_6_2.php
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>February 2, 2010 - RC3 build today.</b></p>
+			<p><b>February 9, 2011 - RC4, final scheduled build.</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -109,7 +109,7 @@ ob_start();
 	          <tr>
 	            <td align="right" height="20"><b>February 2011</b>&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">2</td>
 	            <td align="right" height="20">Wed 08:00 EDT</td>
@@ -136,7 +136,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b>Contribute RC3 build to Helios SR2 train</b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>         
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">9</td>
 	            <td align="right" height="20">Wed 08:00 EDT</td>
