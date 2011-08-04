d54f6fc80b3f5599adbbf3d3cfac1a89c0b27f41
dmegert <dmegert>
2011-08-04 12:43:20 +0000

    Added 3.7.1 freeze plan.

--- development/index.php
+++ development/index.php
@@ -38,6 +38,9 @@ ob_start();
 			<ul>
 		      <li><a href="http://www.eclipse.org/projects/project-plan.php?planurl=http://www.eclipse.org/eclipse/development/plans/eclipse_project_plan_4_2.xml"> Eclipse Project Juno 4.2 Plan</a><br>
 		        This document lays out the feature and API set for the Indigo 4.2 release of the Eclipse Project.</li>
+		      <li><a href="plans/freeze_plan_3_7_1.php"> Eclipse Project Indigo SR1 Endgame Plan</a><br>
+				Information about how we plan to wrap up Helios Service Release 1 (version 3.7.1).</li>
+
 		    </ul>
       	</div>
 		<div class="homeitem3col">
