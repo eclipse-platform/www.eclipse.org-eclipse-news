41e8d335a619a4fe7fced643e66347692038ffb1
dmegert <dmegert>
2012-04-03 07:56:32 +0000

    Removed Ankur

--- team-leaders.php
+++ team-leaders.php
@@ -5,7 +5,7 @@
 	# team-leaders.php
 	#
 	# Author: 		
-	# Date:			2011-11-16
+	# Date:			2012-04-03
 	#
 	# Description: Details about the Eclipse Project PMC
 	#
@@ -118,7 +118,7 @@ ob_start();
 			<ul>
 				<li><a href="http://www.eclipse.org/platform/">Platform</a><br> -- <a href="#McQ">Mike Wilson</a></li>
 				<li><a href="http://www.eclipse.org/jdt/" target="_top">JDT</a><br> -- <a href="#dani">Dani Megert</a></li>
-				<li><a href="http://www.eclipse.org/pde/">PDE</a><br> -- Ankur Sharma and Curtis Windatt</a></li>
+				<li><a href="http://www.eclipse.org/pde/">PDE</a><br> -- Curtis Windatt</a></li>
 				<li><a href="http://www.eclipse.org/e4/" target="_top">e4</a><br> -- <a href="#McQ">Mike Wilson</a></li>
 				<li><a href="http://www.eclipse.org/eclipse/incubator/">Eclipse Incubator</a><br> -- <a href="#McQ">Mike Wilson</a></li>
       		</ul>				
