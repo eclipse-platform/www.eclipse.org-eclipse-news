716a112bc9ad1620d9f4e73dc699903702a7d0ba
dmegert <dmegert>
2012-05-14 12:25:01 +0000

    Update for RC1

--- development/plans/freeze_plan_4_2.php
+++ development/plans/freeze_plan_4_2.php
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>April 30, 2012 - working towards M7/RC0</b></p>
+			<p><b>May 14, 2012 - working towards RC1</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -58,7 +58,7 @@ ob_start();
 	          <tr>
 	            <td align="right" height="20"><b>May 2012</b>&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20"></td>
 	            <td align="right" height="20"></td>
@@ -76,16 +76,21 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#TestPassBeforeRC1"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> details</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	           <tr>
-	            <td height="30"></td>
-	          </tr>
+	          <tr class="current">
+	            <td height="20"></td>
+	            <td align="right" height="20">8</td>
+	            <td align="right" height="20">Tue 08:00 EDT</td>
+	            <td height="20">&nbsp;&nbsp;<b>Start fix pass</b></td>
+	            <td height="20">&nbsp;&nbsp;</td>
+	            <td height="20">&nbsp;&nbsp;<b><a href="#FixPassAfterRC0"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> rules</a></b></td>
+	            <td height="20">&nbsp;&nbsp;</td>
 	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">17</td>
 	            <td align="right" height="20">Thu 08:00 EDT</td>
 	            <td height="20">&nbsp;&nbsp;<b>Release Candidate 1 build</b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
-	            <td height="20">&nbsp;&nbsp;<b><a href="#FixPassAfterRC0"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> rules</a></b></td>
+	            <td height="20">&nbsp;&nbsp;<b><a href="#RC0"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> goals</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
 	           <tr>
