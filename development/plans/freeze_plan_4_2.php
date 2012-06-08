a2b89813aef78ae5c2e0621875626e48bc0d8938
dmegert <dmegert>
2012-06-08 06:55:19 +0000

    Adjusted 'current'

--- development/plans/freeze_plan_4_2.php
+++ development/plans/freeze_plan_4_2.php
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>June 5, 2012 - RC4 fix pass</b></p>
+			<p><b>June 8, 2012 - sign-off for RC4</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -171,7 +171,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#TestPassUsingRC3"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> details</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">5</td>
 	            <td align="right" height="20">Tue 08:00 EDT</td>
@@ -180,7 +180,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#FixPassAfterRC3"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> rules</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">6</td>
 	            <td align="right" height="20">Wed 19:00 EDT</td>
