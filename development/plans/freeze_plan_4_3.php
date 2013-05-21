640950a929dfefebb3c1d3cfa5bf06582edd0ba3
Dani Megert <dmegert>
2013-05-21 12:50:55 +0200

    Moved current

--- development/plans/freeze_plan_4_3.php
+++ development/plans/freeze_plan_4_3.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		
-	# Date:			2013-05-02
+	# Date:			2013-05-21
 	#
 	# Description: Eclipse project Kepler release freeze plan
 	#
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>May 2, 2013 - working towards M7/RC0</b></p>
+			<p><b>May 21, 2013 - RC2 fix pass</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -105,7 +105,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#TestPassBeforeRC2"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> details</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">20</td>
 	            <td align="right" height="20">Mon 08:00 EDT</td>
@@ -195,7 +195,7 @@ ob_start();
 			   <tr>
 	            <td height="20"></td>
 	          </tr>
-			  <tr class="current">
+			  <tr>
 	            <td height="20"></td>
 				<td align="right" height="20"></td>
 	            <td align="right" height="20"></td>
