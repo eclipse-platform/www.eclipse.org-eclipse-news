55f51e59ba8cbe95e3ecfd429c782920bef82a61
Dani Megert <dmegert>
2013-06-11 13:07:51 +0200

    Updated status and current

--- development/plans/freeze_plan_4_3.php
+++ development/plans/freeze_plan_4_3.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		
-	# Date:			2013-06-05
+	# Date:			2013-06-11
 	#
 	# Description: Eclipse project Kepler release freeze plan
 	#
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>June 6, 2013 - Release Candidate 4 sign-off</b></p>
+			<p><b>June 10, 2013 - any changes need PMC approval</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -180,7 +180,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#FixPassAfterRC3"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> rules</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">5</td>
 	            <td align="right" height="20">Wed 20:00 EDT</td>
@@ -195,7 +195,7 @@ ob_start();
 			   <tr>
 	            <td height="20"></td>
 	          </tr>
-			  <tr>
+			  <tr class="current">
 	            <td height="20"></td>
 				<td align="right" height="20"></td>
 	            <td align="right" height="20"></td>
