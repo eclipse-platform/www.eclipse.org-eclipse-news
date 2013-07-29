d3644feec9b849363b8d06b2bf261f8ef468fc89
John Arthorne <John_Arthorne@ca.ibm.com>
2013-07-29 13:35:47 -0400

    Added Orion 3.0 IP log

--- development/project-log-files/index.php
+++ development/project-log-files/index.php
@@ -76,6 +76,7 @@ ob_start();
 			Note the auto-generated log has not been reviewed for accuracy or approved by the Eclipse Foundation legal team.
 			<h4>IP Logs For Past Releases</h4>
 			<ul>
+				<li><a href="/orion/reviews/eclipse-orion-3-0-log.html">Orion 3.0 release</a></li>
 				<li><a href="eclipse-orion-0-4-log.html">Orion 0.4 release</a></li>
 				<li><a href="eclipse-orion-0-3-log.pdf">Orion 0.3 release</a></li>
 				<li><a href="eclipse-orion-creation-release-0-2-log.pdf">Orion creation and 0.2 release</a></li>
