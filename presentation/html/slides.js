c0671d994f7ea56022f4343f939047d7f0adc5bb
John Arthorne <John_Arthorne@ca.ibm.com>
2014-03-07 16:05:29 -0500

    Switch to serve content over https

--- presentation/html/slides.js
+++ presentation/html/slides.js
@@ -11,7 +11,7 @@
   URL: http://code.google.com/p/html5slides/
 */
 
-var PERMANENT_URL_PREFIX = 'http://eclipse.org/eclipse/presentation/html/';
+var PERMANENT_URL_PREFIX = 'https://eclipse.org/eclipse/presentation/html/';
 
 var SLIDE_CLASSES = ['far-past', 'past', 'current', 'next', 'far-next'];
 
