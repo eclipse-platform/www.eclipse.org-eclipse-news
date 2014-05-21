4b1958baa6cd08f480b8d1e09e53814a9d233bda
Dani Megert <dmegert>
2014-05-21 10:24:30 +0200

    Added 4.4 migration guide to web

---
+++ development/porting/eclipse_4_4_porting_guide.php
@@ -0,0 +1,12 @@
+<?php  																														
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
+$App 	= new App();	
+$Nav	= new Nav();	
+$Menu 	= new Menu();
+/* un-comment to use 100% of the content box
+$right_nav = FALSE;
+*/	
+$keyword = 'Eclipse 4.4, API contract compatibility, migrating 4.3 plug-ins to 4.4';
+require_once($_SERVER['DOCUMENT_ROOT'] ."/eclipse/development/nova/_projectCommon.php");
