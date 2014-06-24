80e98913ebd72226759c7209103f2097090de30a
David Williams <david_williams@us.ibm.com>
2014-06-23 21:23:40 -0400

    Bug 437693 - Consolidate "new and noteworthy" documents to one document

--- news/4.4/final/eclipse_news_4.4.php
+++ news/4.4/final/eclipse_news_4.4.php
@@ -1,4 +1,5 @@
 <?php  																														
+$SERVER_NAME="localhost";
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
@@ -7,4 +8,4 @@ $Nav	= new Nav();
 $Menu 	= new Menu();
 $right_nav = FALSE;	
 $keyword = 'news, new and noteworthy, eclipse';
-require_once($_SERVER['DOCUMENT_ROOT'] ."/eclipse/development/nova/_projectCommon.php");
+require("newsCommon.php");
