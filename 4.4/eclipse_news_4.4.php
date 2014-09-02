c2c56ed35d094e364208e0a2d38e92dd1ee142f9
David Williams <david_williams@us.ibm.com>
2014-09-02 20:49:10 +0200

    Bug 437693 - Consolidate "new and noteworthy" documents to one document

---
+++ 4.4/eclipse_news_4.4.php
@@ -0,0 +1,11 @@
+<?php  																														
+$SERVER_NAME="localhost";
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
+$App 	= new App();	
+$Nav	= new Nav();	
+$Menu 	= new Menu();
+$right_nav = FALSE;	
+$keyword = 'news, new and noteworthy, eclipse';
+require("newsCommon.php");
---
+++ 4.4/newsCommon.php
@@ -0,0 +1,47 @@
+<?php
+function innerHTML($el) {
+	$doc = new DOMDocument();
+	$doc->appendChild($doc->importNode($el, TRUE));
+	$html = trim($doc->saveHTML());
+	$tag = $el->nodeName;
+	$html =  preg_replace('@^<' . $tag . '[^>]*>|</' . $tag . '>$@', '', $html);
+	//update relative links
+	// TODO: is is required?
+	//return preg_replace('~href="(.*)_4.4/([^"]+).html"~', 'href="${1}4.4/${2}.php"', $html);
+	return $html;
+}
+
+if(!isset($keyword) OR $keyword == ""){
+	$keyword = "eclipse 4.4, eclipse, new and noteworthy";
+}
+
+$file = basename($App->getScriptName(), ".php").'.html';
+
+$doc = new DOMDocument;
+$doc->loadhtmlfile($file);
+
+$body = $doc->getElementsByTagName('body')->item(0);
+$body = innerHTML($body);
+
+$title = $doc->getElementsByTagName('title')->item(0);
+$title = innerHTML($title);
+#
+# Begin: page-specific settings.  Change these.
+$pageTitle 		= $title;
+$pageKeywords	= $keyword;
+$pageAuthor		= "";
+
+
+$html = $body;
+//$html .='</div></div>';
+
+$html = mb_convert_encoding($html, "HTML-ENTITIES", "auto");
+$App->Promotion = FALSE;	
+
+// if theme is not provided by caller, we specify "current one", explicitly hard-coded
+if (! isset($theme)) {
+  $theme = "solstice";
+}
+
+$App->AddExtraHtmlHeader('<link rel="stylesheet" href="default_style.css" charset="ISO-8859-1" type="text/css">');
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
