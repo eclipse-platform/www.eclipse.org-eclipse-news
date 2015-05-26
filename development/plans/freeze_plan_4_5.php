3de5904d00457d2e083eef39386270c651f34599
Dani Megert <dmegert>
2015-05-26 09:00:16 +0200

    Updated status

--- development/plans/freeze_plan_4_5.php
+++ development/plans/freeze_plan_4_5.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		
-	# Date:			2015-05-22
+	# Date:			2015-05-26
 	#
 	# Description: Eclipse project Mars release freeze plan
 	#
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>May 22 - Submit RC2 to release train</b></p>
+			<p><b>May 26 - Start RC3 fix pass</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -133,7 +133,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#RC2"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> goals</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">22</td>
 	            <td align="right" height="20">Fri</td>
@@ -154,7 +154,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#TestPassUsingRC2"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> details</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">26</td>
 	            <td align="right" height="20">Tue 08:00 EDT</td>
