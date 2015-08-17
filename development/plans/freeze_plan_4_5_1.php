ba16256756fca43de5fd02e8b5c4d990df65baa8
Dani Megert <dmegert>
2015-08-17 14:00:57 +0200

    Updated status

--- development/plans/freeze_plan_4_5_1.php
+++ development/plans/freeze_plan_4_5_1.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		
-	# Date:			2015-08-12
+	# Date:			2015-08-17
 	#
 	# Description: Eclipse project 4.5.1 release freeze plan
 	#
@@ -45,7 +45,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>August 12, 2015 - Release Candidate 1 build.</b></p>
+			<p><b>August 17, 2015 - Working towards RC2.</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -78,7 +78,7 @@ ob_start();
 			  <tr>
 	            <td class="spacer">&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td></td>
 	            <td class="date">12</td>
 	            <td class="date">Wed 08:00 EDT</td>
@@ -97,7 +97,7 @@ ob_start();
 			  <tr>
 	            <td class="spacer">&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td></td>
 	            <td class="date">26</td>
 	            <td class="date">Wed 08:00 EDT</td>
