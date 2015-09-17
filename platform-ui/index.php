73bddd438904401c7a1440dd7de6dace7c4e8ba6
Markus Keller <markus_keller@ch.ibm.com>
2015-09-17 14:20:40 +0200

    reverted unapproved endorsement, see bug 105948 comment 28

--- platform-ui/index.php
+++ platform-ui/index.php
@@ -117,16 +117,6 @@
 			<li><a href="https://wiki.eclipse.org/Platform_UI/How_to_Contribute">How to contribute to Platform UI</a></li>
 			</ul>
 		</div>
-
-		<div class="sideitem">
-			<h6>Thanks to Yourkit</h6>
-			YourKit supports open source projects with its full-featured Java Profiler.
-YourKit, LLC is the creator of <a href="https://www.yourkit.com/java/profiler/index.jsp">YourKit Java Profiler</a>
-and <a href="https://www.yourkit.com/.net/profiler/index.jsp">YourKit .NET Profiler</a>,
-innovative and intelligent tools for profiling Java and .NET applications.
-		</div>
-
-
 	</div>
 </div>
 
