230ed624dc0b374a37aa704ee55ce6a37eaad437
Dani Megert <dmegert>
2016-05-20 08:51:02 +0200

    Updated current

--- development/plans/freeze_plan_4_6.php
+++ development/plans/freeze_plan_4_6.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		
-	# Date:			2016-05-19
+	# Date:			2016-05-20
 	#
 	# Description: Eclipse project Neon release freeze plan
 	#
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>May 18 - Fix pass and release candidate 2 build</b></p>
+			<p><b>May 20 - Submit RC2 to release train</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -124,7 +124,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#FixPassAfterRC1"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> rules</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">18</td>
 	            <td align="right" height="20">Wed 20:00 EDT</td>
@@ -133,7 +133,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#RC2"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> goals</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">20</td>
 	            <td align="right" height="20">Fri</td>
