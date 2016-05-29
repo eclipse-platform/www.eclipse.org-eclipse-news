6713d37b2301d946b48829ad12674ebff87378e2
Dani Megert <dmegert>
2016-05-29 16:26:55 +0200

    Updated current

--- development/plans/freeze_plan_4_6.php
+++ development/plans/freeze_plan_4_6.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		
-	# Date:			2016-05-26
+	# Date:			2016-05-29
 	#
 	# Description: Eclipse project Neon release freeze plan
 	#
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>May 26 - Submit Release Candidate 3 to release train</b></p>
+			<p><b>May 30 - Start 1-day test pass against RC3</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -172,7 +172,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#RC3"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> goals</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">27</td>
 	            <td align="right" height="20">Fri</td>
@@ -181,7 +181,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;</td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">30</td>
 	            <td align="right" height="20">Mon 08:00 EDT</td>
