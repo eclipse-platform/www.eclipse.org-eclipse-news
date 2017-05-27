1d2826e778f5547b552360888e526e12989f46e4
Dani Megert <dmegert>
2017-05-27 17:06:13 +0200

    Updated status and current

--- development/plans/freeze_plan_4_7.php
+++ development/plans/freeze_plan_4_7.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		Dani Megert
-	# Date:			2017-05-26
+	# Date:			2017-05-27
 	#
 	# Description: Eclipse project Oxygen release freeze plan
 	#
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>May 26 - Submit to release train.</b></p>
+			<p><b>May 29 - Start RC3 fix pass.</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -130,7 +130,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;</td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">26</td>
 	            <td align="right" height="20">Fri</td>
@@ -139,7 +139,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;</td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">29</td>
 	            <td align="right" height="20">Mon 08:00 EDT</td>
