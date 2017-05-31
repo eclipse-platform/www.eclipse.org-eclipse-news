c1be2b5bf1d36b1eb00eedc774b42ac3189068c8
Dani Megert <dmegert>
2017-05-31 17:42:24 +0200

    Updated status and current

--- development/plans/freeze_plan_4_7.php
+++ development/plans/freeze_plan_4_7.php
@@ -7,7 +7,7 @@
 	# index.php
 	#
 	# Author: 		Dani Megert
-	# Date:			2017-05-27
+	# Date:			2017-05-31
 	#
 	# Description: Eclipse project Oxygen release freeze plan
 	#
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>May 29 - Start RC3 fix pass.</b></p>
+			<p><b>May 31 - Release Candidate 3 build.</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -139,7 +139,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;</td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">29</td>
 	            <td align="right" height="20">Mon 08:00 EDT</td>
@@ -148,7 +148,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#FixPassAfterRC2"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> details</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">31</td>
 	            <td align="right" height="20">Wed 20:00 EDT</td>
