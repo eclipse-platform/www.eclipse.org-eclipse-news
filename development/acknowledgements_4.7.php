65d960ec3d561448b00c6d66c21571d2a2e809f8
Sarika Sinha <sarika.sinha@in.ibm.com>
2017-06-01 15:09:44 +0530

    Bug 517276 - Oxygen (4.7) Acknowledgements
    Change-Id: I50972b4f73b6109031045e13c414be6b20ec0515

--- development/acknowledgements_4.7.php
+++ development/acknowledgements_4.7.php
@@ -259,24 +259,24 @@ ob_start();
     <table style="width:92%; margin-left:4%; margin-right:4%;">
 
       <tr>
-        <td style="width: 23%;">Sarika&nbsp;Sinha</td>
+        <td style="width: 23%;">Andrey&nbsp;Loskutov</td>
         <td style="width: 23%;">Dani&nbsp;Megert</td>
         <td style="width: 23%;">Markus&nbsp;Keller</td>
-        <td style="width: 23%;">Michael&nbsp;Rennie</td>
+        <td style="width: 23%;">Sarika&nbsp;Sinha</td>
       </tr>
     </table>
 
     <h4>Eclipse JDT</h4>
     <table style="width:92%; margin-left:4%; margin-right:4%;">
       <tr>
+      	<td style="width: 23%;">Andrey&nbsp;Loskutov</td>
         <td style="width: 23%;">Dani&nbsp;Megert</td>
         <td style="width: 23%;">Jayaprakash&nbsp;Arthanareeswaran</td>
         <td style="width: 23%;">Manoj&nbsp;Palat</td>
-        <td style="width: 23%;">Markus&nbsp;Keller</td>
       </tr>
       <tr>
+        <td style="width: 23%;">Markus&nbsp;Keller</td>
         <td style="width: 23%;">Mateusz&nbsp;Matela</td>
-        <td style="width: 23%;">Michael&nbsp;Rennie</td>
         <td style="width: 23%;">Noopur&nbsp;Gupta</td>
         <td style="width: 23%;">Olivier&nbsp;Thomann</td>
       </tr>
