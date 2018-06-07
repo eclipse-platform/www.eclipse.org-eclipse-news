02af49d9ef17c1e2d306e24db75655d05fe02f31
Dani Megert <Daniel_Megert@ch.ibm.com>
2018-06-07 09:55:20 +0200

    Updated status and current

--- development/plans/freeze_plan_4_8.php
+++ development/plans/freeze_plan_4_8.php
@@ -41,7 +41,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>June 6 - Release Candidate 4 build.</b></p>
+			<p><b>June 7 - 1-day test pass against RC4 and sign-off.</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
@@ -196,7 +196,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#FixPassAfterRC3"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> details</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr class="current">
+	          <tr>
 	            <td height="20"></td>
 	            <td align="right" height="20">6</td>
 	            <td align="right" height="20">Wed 20:00 EDT</td>
@@ -205,7 +205,7 @@ ob_start();
 	            <td height="20">&nbsp;&nbsp;<b><a href="#RC4"><img src="../images/jump_in_black.gif" border="0" hspace="3" width="12" height="10"> goals</a></b></td>
 	            <td height="20">&nbsp;&nbsp;</td>
 	          </tr>
-	          <tr>
+	          <tr class="current">
 	            <td height="20"></td>
 	            <td align="right" height="20">7</td>
 	            <td align="right" height="20">Thu</td>
