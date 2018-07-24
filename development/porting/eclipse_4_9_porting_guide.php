92b023f793e7cd9f87ba2d3d2f941e4d8832085a
Dani Megert <Daniel_Megert@ch.ibm.com>
2018-07-24 17:22:25 +0200

    Fixed bug 537058: Initial 4.9 Plan

---
+++ development/porting/eclipse_4_9_porting_guide.php
@@ -0,0 +1,12 @@
+<?php  																														
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
+$App 	= new App();	
+$Nav	= new Nav();	
+$Menu 	= new Menu();
+/* un-comment to use 100% of the content box
+$right_nav = FALSE;
+*/	
+$keyword = 'Eclipse 4.8, API contract compatibility, migrating 4.7 plug-ins to 4.8';
+require_once($_SERVER['DOCUMENT_ROOT'] ."/eclipse/development/nova/_projectCommon.php");
