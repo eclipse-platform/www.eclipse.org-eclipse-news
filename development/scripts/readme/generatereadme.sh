a87761ccb2169efee162441be896c76387273b89
Sarika Sinha <sarika.sinha@in.ibm.com>
2019-05-23 13:11:21 +0530

    Bug 547548 - Creating script for Readme
    Change-Id: I027d05d1ce8b0ebd4569f1094ef88d3d3e577730
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>

---
+++ development/scripts/readme/generatereadme.sh
@@ -0,0 +1,5 @@
+cp  readme_eclipse.html readme_eclipse_old.html
+sed -i -e 's/4\.11/4\.12/g'  readme_eclipse.html
+sed -i -e 's/4_11/4_12/g' readme_eclipse.html
+sed -i -e 's/4\.10/4\.11/g' readme_eclipse.html
+
