5a628cfc77bb8d216badf70c4c1cdedbbd58df79
Sarika Sinha <sarika.sinha@in.ibm.com>
2020-08-26 18:07:42 +0530

    Bug 566299 - Readme file for 4.17
    Change-Id: Ib97cc0dd3821cec511157e609672a9c30849d6cc

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.11/4\.12/g'  readme_eclipse.html
-sed -i -e 's/4_11/4_12/g' readme_eclipse.html
-sed -i -e 's/4\.10/4\.11/g' readme_eclipse.html
+sed -i -e 's/4\.16/4\.17/g'  readme_eclipse.html
+sed -i -e 's/4_16/4_17/g' readme_eclipse.html
+sed -i -e 's/4\.15/4\.16/g' readme_eclipse.html
 
