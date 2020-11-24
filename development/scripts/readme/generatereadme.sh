3a2e2d6fd67172cb15705554f7d1acaeb8c48068
Sarika Sinha <sarika.sinha@in.ibm.com>
2020-11-24 12:29:31 +0530

    Bug 568974 - Readme file for 4.18
    Change-Id: I0166590111665cc874fb380eac51f2c6ce61a52f

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.16/4\.17/g'  readme_eclipse.html
-sed -i -e 's/4_16/4_17/g' readme_eclipse.html
-sed -i -e 's/4\.15/4\.16/g' readme_eclipse.html
+sed -i -e 's/4\.17/4\.18/g'  readme_eclipse.html
+sed -i -e 's/4_17/4_18/g' readme_eclipse.html
+sed -i -e 's/4\.16/4\.17/g' readme_eclipse.html
 
