7a4ff96f122b3c125cc85b2c67121509105d26e1
Sarika Sinha <sarika.sinha@in.ibm.com>
2021-02-21 18:32:02 +0530

    Bug 571388 - Readme file for 4.19
    Change-Id: I7e04f4f4b400c8c3249b65c438007e0f3d87ecca
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.17/4\.18/g'  readme_eclipse.html
-sed -i -e 's/4_17/4_18/g' readme_eclipse.html
-sed -i -e 's/4\.16/4\.17/g' readme_eclipse.html
+sed -i -e 's/4\.18/4\.19/g'  readme_eclipse.html
+sed -i -e 's/4_18/4_19/g' readme_eclipse.html
+sed -i -e 's/4\.17/4\.18/g' readme_eclipse.html
 
