3808fbac47356ae8f7158dcde948d85293917ed6
Sarika Sinha <sarika.sinha@in.ibm.com>
2021-05-25 01:17:08 +0530

    Bug 573733 - Readme file for 4.20
    Change-Id: Iaca734eb61c9d4bea1ff0587dcce2a92749df782

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.18/4\.19/g'  readme_eclipse.html
-sed -i -e 's/4_18/4_19/g' readme_eclipse.html
-sed -i -e 's/4\.17/4\.18/g' readme_eclipse.html
+sed -i -e 's/4\.19/4\.20/g'  readme_eclipse.html
+sed -i -e 's/4_19/4_20/g' readme_eclipse.html
+sed -i -e 's/4\.18/4\.19/g' readme_eclipse.html
 
