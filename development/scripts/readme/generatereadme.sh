c87ef13a5174f36adcd300db99b77c707a049577
Sarika Sinha <sarika.sinha@in.ibm.com>
2021-08-25 11:25:15 +0530

    Bug 575583 - Readme file for 4.21
    Change-Id: I7d94134f4b6c27942c60b653f467acc5b4885ccd

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.19/4\.20/g'  readme_eclipse.html
-sed -i -e 's/4_19/4_20/g' readme_eclipse.html
-sed -i -e 's/4\.18/4\.19/g' readme_eclipse.html
+sed -i -e 's/4\.20/4\.21/g'  readme_eclipse.html
+sed -i -e 's/4_20/4_21/g' readme_eclipse.html
+sed -i -e 's/4\.19/4\.20/g' readme_eclipse.html
 
