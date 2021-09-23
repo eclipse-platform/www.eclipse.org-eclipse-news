f5aeae5096aa3a60f9e3defbd2b140d4a8cf65f9
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2021-09-23 15:02:14 +0530

    Updated project plan
    Change-Id: If3b09dcf51a1370205ccaec4f3600c77c7cfc52e
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- development/plans/freeze_plan_4_21.php
+++ development/plans/freeze_plan_4_21.php
@@ -45,7 +45,7 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 		<div class="homeitem3col">
 			<h3>Status</h3>
-			<p><b>In quiet period</b></p>
+			<p><b>Released</b></p>
       	</div>
 		<div class="homeitem3col">
 			<h3>Detailed Timeline</h3>
