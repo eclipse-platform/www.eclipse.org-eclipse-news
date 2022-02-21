3b7e1edb581a27b259337c76a77343a1a142b588
Sarika Sinha <sarika.sinha@in.ibm.com>
2022-02-21 22:34:42 +0530

    Bug 578868 - Readme file for 4.23
    Change-Id: I31c408430fc6245ba38598d33211fbccd60bdb39

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.21/4\.22/g'  readme_eclipse.html
-sed -i -e 's/4_21/4_22/g' readme_eclipse.html
-sed -i -e 's/4\.20/4\.21/g' readme_eclipse.html
+sed -i -e 's/4\.22/4\.23/g'  readme_eclipse.html
+sed -i -e 's/4_22/4_23/g' readme_eclipse.html
+sed -i -e 's/4\.21/4\.22/g' readme_eclipse.html
 
