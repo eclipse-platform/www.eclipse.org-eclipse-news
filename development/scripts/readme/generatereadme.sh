4c6766817f6f755b972e17e86cb943d0f0bbd62a
Sarika Sinha <sarika.sinha@in.ibm.com>
2022-08-24 18:40:57 +0530

    Readme for 4.25 (#16)
    https://github.com/eclipse-platform/www.eclipse.org-eclipse/issues/15

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.23/4\.24/g'  readme_eclipse.html
-sed -i -e 's/4_23/4_24/g' readme_eclipse.html
-sed -i -e 's/4\.22/4\.23/g' readme_eclipse.html
+sed -i -e 's/4\.24/4\.25/g'  readme_eclipse.html
+sed -i -e 's/4_24/4_25/g' readme_eclipse.html
+sed -i -e 's/4\.23/4\.24/g' readme_eclipse.html
 
