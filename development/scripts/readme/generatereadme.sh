4a5c8507e94d4076a5a1fae6685e6ac102e0f102
Shee43 <122248524+Shee43@users.noreply.github.com>
2023-02-28 22:01:33 +0530

    4.27 Readme updated (#30)

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.25/4\.26/g'  readme_eclipse.html
-sed -i -e 's/4_25/4_26/g' readme_eclipse.html
-sed -i -e 's/4\.24/4\.25/g' readme_eclipse.html
+sed -i -e 's/4\.26/4\.27/g'  readme_eclipse.html
+sed -i -e 's/4_26/4_27/g' readme_eclipse.html
+sed -i -e 's/4\.25/4\.26/g' readme_eclipse.html
 
