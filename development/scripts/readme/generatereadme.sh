da59e2581d7ddcbade7897058367da0057a52780
Sheena <122248524+Shee43@users.noreply.github.com>
2023-05-30 10:59:06 -0700

    Readme file for 4.28 (#36)
    https://github.com/eclipse-platform/eclipse.platform.releng/issues/233

--- development/scripts/readme/generatereadme.sh
+++ development/scripts/readme/generatereadme.sh
@@ -1,5 +1,5 @@
 cp  readme_eclipse.html readme_eclipse_old.html
-sed -i -e 's/4\.26/4\.27/g'  readme_eclipse.html
-sed -i -e 's/4_26/4_27/g' readme_eclipse.html
-sed -i -e 's/4\.25/4\.26/g' readme_eclipse.html
+sed -i -e 's/4\.27/4\.28/g'  readme_eclipse.html
+sed -i -e 's/4_27/4_28/g' readme_eclipse.html
+sed -i -e 's/4\.26/4\.27/g' readme_eclipse.html
 
